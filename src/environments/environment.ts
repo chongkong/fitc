a3fc98eb9448042787e89ea9d0b3fb41ab4dc5eb
Jongbin Park <chongkong94@gmail.com>
2019-11-09 03:27:11 +0900

    Add Angular fire and firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDPVoxps6UMXllT2S9mw7F6Osd04k2hcr8",
+    authDomain: "foosball-seo.firebaseapp.com",
+    databaseURL: "https://foosball-seo.firebaseio.com",
+    projectId: "foosball-seo",
+    storageBucket: "foosball-seo.appspot.com",
+    messagingSenderId: "306936162920",
+    appId: "1:306936162920:web:902073f801ed6c8757a9dd"
+  },
 };
 
 /*
