db49e71c4b85b20f74e6be9212fd7e065f836607
Jongbin Park <chongkong94@gmail.com>
2019-11-09 15:31:36 +0900

    Implement PreviousRoutesService

--- src/app/services/previous-routes.service.ts
+++ src/app/services/previous-routes.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
+import { filter, pairwise } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreviousRoutesService {
+  private _previousUrl?: string;
+  
+  constructor(public router: Router) { 
+    this.router.events
+        .pipe(filter(event => event instanceof RoutesRecognized))
+        .pipe(pairwise())
+        .subscribe(([r1, r2]: [RoutesRecognized, RoutesRecognized]) => {
+          this._previousUrl = r1.urlAfterRedirects;
+        });
+  }
 
-  constructor() { }
+  get previousUrl() {
+    return this._previousUrl;
+  }
 }
