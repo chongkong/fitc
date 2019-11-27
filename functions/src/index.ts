9def8127ef5fb190706eea4d43e7e7ace3a2ba3b
Jongbin Park <chongkong94@gmail.com>
2019-11-27 14:20:08 +0900

    Remove createGameRecord firebase function call

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,3 @@
 export { onUserCreate } from './triggers/auth';
 export { onGameRecordCreate } from './triggers/game-record';
 export { onEventCreate } from './triggers/event';
-
-export { createGameRecord } from './http/create-game-record';
