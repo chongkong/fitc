d7af3d48ca5557574b71727ebd05317202c654dc
Jongbin Park <chongkong94@gmail.com>
2019-11-30 17:10:29 +0900

    Fix wrong constant values

--- functions/src/constant.ts
+++ functions/src/constant.ts
@@ -1,5 +1,5 @@
 // Default history size.
-export const DEFAULT_HISTORY_SIZE = 50;
+export const DEFAULT_HISTORY_SIZE = 100;
 
 // Maximum time in milliseconds that the system recognizes as consecutive plays.
-export const CONSECUTIVE_PLAY_THRESHOLD_MILLIS = 60 * 60 * 3600;
+export const CONSECUTIVE_PLAY_THRESHOLD_MILLIS = 60 * 60 * 1000;
