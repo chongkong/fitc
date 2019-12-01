84afb014f24a3652e1af1b7118dbf3b1171b46c2
Jiwon Shin <sarah95071626@gmail.com>
2019-12-01 16:26:37 +0000

    Make main component use material angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AngularFireModule } from "@angular/fire";
 import { FirestoreSettingsToken } from "@angular/fire/firestore";
 import { AngularFireAuthGuardModule } from "@angular/fire/auth-guard";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "src/app/app-routing.module";
 import { AppComponent } from "src/app/app.component";
@@ -21,7 +21,7 @@ import { MainModule } from "./pages/main/main.module";
     AngularFireAuthGuardModule,
     LoginModule,
     MainModule,
-    NoopAnimationsModule
+    BrowserAnimationsModule
   ],
   providers: [
     {
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatListModule } from "@angular/material/list";
 
 import { MainComponent } from "./main.component";
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, AngularFirestoreModule, FormsModule]
+  imports: [
+    AngularFirestoreModule,
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatListModule
+  ]
 })
 export class MainModule {}
