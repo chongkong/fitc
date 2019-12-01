88c03269ed9d2a0445de6f66ccb1dff48a8ccfff
Jongbin Park <chongkong94@gmail.com>
2019-12-02 03:16:11 +0900

    Update scripts with old schema (PlayerStats) as well as formatting

--- scripts/init_emulator.py
+++ scripts/init_emulator.py
@@ -1,67 +1,61 @@
 import json
 import os
 
-import firebase_admin 
+import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 
 def get_firestore_emulator():
-  # Firestore client uses emulator if environment variable is set.
-  # https://github.com/googleapis/google-cloud-python/blob/master/firestore/google/cloud/firestore_v1/client.py#L121
-  os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
-  firebase_admin.initialize_app(
-    credential=credentials.Certificate('../serviceAccountKey.json'))
-  return firestore.client()
+    # Firestore client uses emulator if environment variable is set.
+    # https://github.com/googleapis/google-cloud-python/blob/master/firestore/google/cloud/firestore_v1/client.py#L121
+    os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
+    app = firebase_admin.initialize_app(
+        credential=credentials.Certificate("../serviceAccountKey.json"))
+    return firestore.client(app)
 
 
 def init_firestore():
-  db = get_firestore_emulator()
-  print(db)
-  batch = db.batch()
+    db = get_firestore_emulator()
+    print(db)
+    batch = db.batch()
 
-  def add_player_stats(ldap):
-    print('Add playerStats {}'.format(ldap))
-    batch.set(db.collection('playerStats').document(ldap), {
-      'totalWins': 0,
-      'totalLoses': 0,
-      'mostWinStreaks': 0,
-      'recentGames': '',
-      'perSeason': {},
-      'asOpponent': {},
-      'asTeammate': {},
-    })
+    def add_player(ldap, name, level):
+        print('Add player {}'.format(ldap))
+        batch.set(db.collection('players').document(ldap), {
+            'name': name,
+            'ldap': ldap,
+            'level': level
+        })
+        print('Add playerStats {}'.format(ldap))
+        batch.set(db.collection('playerStats').document(ldap), {
+            'totalWins': 0,
+            'totalLoses': 0,
+            'mostWinStreaks': 0,
+            'recentGames': '',
+        })
 
-  def add_player(ldap, name, level):
-    print('Add player {}'.format(ldap))
-    batch.set(db.collection('players').document(ldap), {
-      'name': name,
-      'ldap': ldap,
-      'level': level
-    })
-    add_player_stats(ldap=ldap)
-  
-  def add_table(table_id, name, recent_players):
-    print('Add table {}'.format(table_id))
-    batch.set(db.collection('tables').document(table_id), {
-      'name': name,
-      'recentPlayers': recent_players
-    })
-  
-  add_player(ldap='jjong', name='Jongbin Park', level=2)
-  add_player(ldap='hyeonjilee', name='Hyeonji Lee', level=3)
-  add_player(ldap='shinjiwon', name='Jiwon Shin', level=2)
-  add_player(ldap='anzor', name='Anzor Balkar', level=4)
-  add_player(ldap='hdmoon', name='Hyundo Moon', level=2)
+    def add_table(table_id, name, recent_players):
+        print('Add table {}'.format(table_id))
+        batch.set(db.collection('tables').document(table_id), {
+            'name': name,
+            'recentPlayers': recent_players
+        })
 
-  add_table(
-    table_id='default',
-    name='GFC 24F Bonzini',
-    recent_players=['jjong', 'hyeonjilee', 'shinjiwon', 'anzor', 'hdmoon']
-  )
+    add_player(ldap='jjong', name='Jongbin Park', level=2)
+    add_player(ldap='hyeonjilee', name='Hyeonji Lee', level=3)
+    add_player(ldap='shinjiwon', name='Jiwon Shin', level=2)
+    add_player(ldap='anzor', name='Anzor Balkar', level=4)
+    add_player(ldap='hdmoon', name='Hyundo Moon', level=2)
 
-  batch.commit()
+    add_table(
+        table_id='default',
+        name='Default',
+        recent_players=['jjong', 'hyeonjilee', 'shinjiwon', 'anzor', 'hdmoon']
+    )
+
+    batch.commit()
 
 
 if __name__ == '__main__':
-  init_firestore()
+    init_firestore()
--- scripts/promo_threshold_gen.py
+++ scripts/promo_threshold_gen.py
@@ -7,84 +7,85 @@ import scipy.integrate
 
 
 def integrate(scalar_fn, a, b):
-  result = scipy.integrate.quad(scalar_fn, a, b)
-  return result[0]
+    result = scipy.integrate.quad(scalar_fn, a, b)
+    return result[0]
 
 
 @functools.lru_cache(maxsize=1024)
 def binom(n, k):
-  """Returns bionmial coefficient n!/(n-k)!k!"""
-  if 2*k > n:
-    k = n - k
-  if k == 0:
-    return 1
-  return binom(n-1, k-1) + binom(n-1, k)
+    """Returns bionmial coefficient n!/(n-k)!k!"""
+    if 2*k > n:
+        k = n - k
+    if k == 0:
+        return 1
+    return binom(n-1, k-1) + binom(n-1, k)
 
 
 def binomial_pmf(p, n, k):
-  """Returns probability of getting `k` positive out of `n` trials
-  when success probability is `p`."""
-  return binom(n, k) * pow(p, k) * pow (1-p, n-k)
+    """Returns probability of getting `k` positive out of `n` trials
+    when success probability is `p`."""
+    return binom(n, k) * pow(p, k) * pow(1-p, n-k)
 
 
 @np.vectorize
 def binomial_test(threshold, wins, loses):
-  """Returns probabiliy of winrate >= threshold given number of wins
-  and loses."""
-  binomial_pdf = functools.partial(binomial_pmf, n=wins + loses, k=wins)
-  return (integrate(binomial_pdf, a=threshold, b=1) 
-          / integrate(binomial_pdf, a=0, b=1))
+    """Returns probabiliy of winrate >= threshold given number of wins
+    and loses."""
+    binomial_pdf = functools.partial(binomial_pmf, n=wins + loses, k=wins)
+    return (integrate(binomial_pdf, a=threshold, b=1)
+            / integrate(binomial_pdf, a=0, b=1))
 
 
 @np.vectorize
 def required_wins_to_claim_winrate(num_games, winrate, confidence):
-  """Return number of games won to claim winrate >= winrate 
-  given total number of games with given confidence."""
-  winrate = np.ones([num_games + 1], dtype=np.float64) * winrate
-  wins = np.arange(num_games + 1)
-  loses = num_games - wins
-  result = binomial_test(winrate, wins, loses)
-  if any(result):
-    return np.argmax(result > confidence)
-  else:
-    return -1
+    """Return number of games won to claim winrate >= winrate 
+    given total number of games with given confidence."""
+    winrate = np.ones([num_games + 1], dtype=np.float64) * winrate
+    wins = np.arange(num_games + 1)
+    loses = num_games - wins
+    result = binomial_test(winrate, wins, loses)
+    if any(result):
+        return np.argmax(result > confidence)
+    else:
+        return -1
 
 
 def main(
-  winrate,
-  confidence,
-  output_format='js',  # Output format.
-  window_size=50,  # Number of recent games to decide promotion.
-  min_games=10,  # Minimal number of games to play to be a promo candidate.
+    winrate,
+    confidence,
+    output_format='js',  # Output format.
+    window_size=100,  # Number of recent games to decide promotion.
+    min_games=10,  # Minimal number of games to play to be a promo candidate.
 ):
-  promo_threshold = required_wins_to_claim_winrate(
-    num_games=np.arange(window_size + 1),
-    winrate=winrate,
-    confidence=confidence
-  )
-  promo_threshold[:min_games] = -1
+    promo_threshold = required_wins_to_claim_winrate(
+        num_games=np.arange(window_size + 1),
+        winrate=winrate,
+        confidence=confidence
+    )
+    promo_threshold[:min_games] = -1
 
-  if output_format == 'js':
-    print('\n'.join([
-      'const PROMO_THRESHOLDS = [',
-      *[
-        '  ' + ('{:2d}, ' * min(10, len(promo_threshold[10*i:])))
-          .format(*promo_threshold[10*i:10*(i + 1)])
-          .rstrip()
-        for i in range(0, (len(promo_threshold) + 9) // 10)
-      ],
-      '];'
-    ]).replace(',\n]', '\n]'))
+    if output_format == 'js':
+        print('\n'.join([
+            'const PROMO_THRESHOLDS = [',
+            *[
+              '  ' + ('{:2d}, ' * min(10, len(promo_threshold[10*i:])))
+              .format(*promo_threshold[10*i:10*(i + 1)])
+              .rstrip()
+              for i in range(0, (len(promo_threshold) + 9) // 10)
+              ],
+            '];'
+        ]).replace(',\n]', '\n]'))
 
-  elif output_format == 'md':
-    print('\n'.join([
-      '| Required Wins | Games Played |',
-      '| ============= | ============ |',
-      *[
-        '| {:13d} | {:12d} |'.format(wins, total)
-        for wins, total in zip(promo_threshold, np.arange(window_size + 1))
-      ]
-    ]))
+    elif output_format == 'md':
+        print('\n'.join([
+            '| Required Wins | Games Played |',
+            '| ============= | ============ |',
+            *[
+              '| {:13d} | {:12d} |'.format(wins, total)
+              for wins, total in zip(promo_threshold, np.arange(window_size + 1))
+              ]
+        ]))
+
 
 if __name__ == '__main__':
-  fire.Fire(main)
+    fire.Fire(main)
