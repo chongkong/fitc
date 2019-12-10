d245ed8b9bef62cecce8d3d305dc9b2a25a21f09
Jongbin Park <chongkong94@gmail.com>
2019-12-11 01:55:48 +0900

    Make GameRecord timestamp timeago

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { PlayerChipComponent } from "./player-chip/player-chip.component";
-import { GameRecordComponent } from "./game-record/game-record.component";
-import { PlayerSelectDialogComponent } from "./player-select-dialog/player-select-dialog.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
+import { TimeagoModule } from "ngx-timeago";
+
+import { PlayerChipComponent } from "./player-chip/player-chip.component";
+import { GameRecordComponent } from "./game-record/game-record.component";
+import { PlayerSelectDialogComponent } from "./player-select-dialog/player-select-dialog.component";
 
 @NgModule({
   declarations: [
@@ -13,7 +15,13 @@ import { MatButtonModule } from "@angular/material/button";
     GameRecordComponent,
     PlayerSelectDialogComponent
   ],
-  imports: [CommonModule, FontAwesomeModule, MatDialogModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    MatDialogModule,
+    MatButtonModule,
+    TimeagoModule
+  ],
   exports: [
     PlayerChipComponent,
     GameRecordComponent,
